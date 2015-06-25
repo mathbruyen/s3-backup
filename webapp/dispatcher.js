5825831f47ccebe12cf2e29b012765cc0a47dafd
Mathieu Bruyen <code@mais-heu>
2015-06-25 08:51:37 +0200

    Dispatcher ensures stores run-to-completion

--- webapp/dispatcher.js
+++ webapp/dispatcher.js
@@ -3,10 +3,20 @@
 module.exports = () => {
 
   var subscribers = [];
+  var running = false;
 
   function dispatch(action) {
-    for (var subscriber of subscribers) {
-      subscriber(action);
+    if (running) {
+      throw new Error('Cannot dispatch ' + JSON.stringify(action) + ' because an other action is already in progress');
+    }
+    running = true;
+    try {
+      console.log('Action:', action);
+      for (var subscriber of subscribers) {
+        subscriber(action);
+      }
+    } finally {
+      running = false;
     }
   }
 
